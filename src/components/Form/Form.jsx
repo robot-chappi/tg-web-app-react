3c68fcf7eb9c3aaaa57f1cf5e94b1521b46cf2c5
robot-chappi <danuul123fedoscov@gmail.com>
2023-02-08 19:27:27 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,7 +16,7 @@ const Form = () => {
     }, [])
 
     useEffect(() => {
-        if (!street || !country) {
+        if(!street || !country) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
@@ -24,21 +24,35 @@ const Form = () => {
     }, [country, street])
 
     const onChangeCountry = (e) => {
-      setCountry(e.target.value)
-    };
+        setCountry(e.target.value)
+    }
+
     const onChangeStreet = (e) => {
         setStreet(e.target.value)
-    };
+    }
+
     const onChangeSubject = (e) => {
         setSubject(e.target.value)
-    };
+    }
 
     return (
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
-            <input className={'input'} type="text" placeholder={'Страна'} value={country} onChange={onChangeCountry}/>
-            <input className={'input'} type="text" placeholder={'Улица'} value={street} onChange={onChangeStreet}/>
-            <select className={'select'} value={subject} onChange={onChangeSubject}>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
